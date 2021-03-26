f252882f5680d05870179a1232d18ff20dfe2472
Po-Hsun Tseng <zengbs@gmail.com>
2021-03-26 20:10:42 +0800

    update

--- hw1/P4_Calculator/main.c
+++ hw1/P4_Calculator/main.c
@@ -2,24 +2,37 @@
 #include<stdio.h>
 #include<stdint.h> // The maximum size of size_t is provided via SIZE_MAX
 
-#define MAX_STRING 10
+#define MAX_STRING_SIZE 1e2
 
 
 int top = -1; /*the index of the last element in stack*/
+
 void push(char str[], char data, int line);
 char pop(char str[], int line);
 void display(char str[], int line);
+char peek(char str[], int line);
 
 int main(){
 
-  char *input;
+  char *input, *stack;
   
-  input = (char*)malloc((size_t)(MAX_STRING)*sizeof(char));
+  input = (char*)malloc((size_t)(MAX_STRING_SIZE)*sizeof(char));
+  stack = (char*)malloc((size_t)(MAX_STRING_SIZE)*sizeof(char));
   if (input == NULL) { printf("input is NULL at %d!\n", __LINE__); exit(0); }
   
   scanf( "%s", input);
 
 
+  push( stack, input[0], __LINE__ );
+  push( stack, input[1], __LINE__ );
+  push( stack, input[2], __LINE__ );
+  push( stack, input[3], __LINE__ );
+  push( stack, input[4], __LINE__ );
+
+    
+  //printf("stack[%d]=%c\n", top, peek(stack, __LINE__));
+  
+  display(stack, __LINE__); 
 
 
 
@@ -33,7 +46,7 @@ int main(){
 
 void push(char str[], char data, int line)
 {
-   if (top == MAX_STRING-1 ){
+   if (top == MAX_STRING_SIZE-1 ){
        printf("stack overflow at %d!\n", line); exit(0);
    }
    else{
@@ -53,13 +66,13 @@ char pop(char str[], int line)
    }
 }
 
-void display(char *str, int line)
+void display(char str[], int line)
 {
    if (top == -1 ){
        printf("stack is empty at %d!\n", line);
    }
    else{
-      while (top != 0){
+      while (top != -1){
         printf("%c", str[top]);
         top--;
       }
@@ -68,10 +81,10 @@ void display(char *str, int line)
 
 }
 
-int peek(char str[], int line)
+char peek(char str[], int line)
 {
    if (top == -1 ){
-       printf("stack is empty at %d!\n", line); return -1; exit(0);
+       printf("stack is empty at %d!\n", line); return "\0"; exit(0);
    }
    else{
       return  str[top];
