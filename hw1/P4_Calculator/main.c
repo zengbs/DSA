96f66620db4031a2d6065ec79ffc647cf06def1f
Po-Hsun Tseng <zengbs@gmail.com>
2021-03-26 17:15:08 +0800

    update

--- hw1/P4_Calculator/main.c
+++ hw1/P4_Calculator/main.c
@@ -2,26 +2,19 @@
 #include<stdio.h>
 #include<stdint.h> // The maximum size of size_t is provided via SIZE_MAX
 
-#define MAX_STRING 1e2
+#define MAX_STRING (1e2)
 
 
-struct st_Stack_char {
-   int  maxsize; /*the maximum allowed size*/
-   int  top;     /*the size of stack*/
-   char *item;    
-};
-
-
-void initializeStack(struct st_Stack_char stack, int maxsize){
-   stack.maxsize = maxsize;
-   stack.top     = 0;
-   stack.items   = NULL;
-}
+void push(char [], char data);
+char pop(char []);
+char peek(char []);
+void display(char []);
 
 
 int main(){
 
   char *input;
+  int top = -1; /*the index of the last element in stack*/
   
   input = (char*)malloc((size_t)(MAX_STRING)*sizeof(char));
   if (input == NULL) printf("input is NULL at %d!\n", __LINE__); exit(0);
@@ -30,9 +23,34 @@ int main(){
 
 
 
+
+
+
   
   if (input == NULL) printf("input is NULL at %d!\n", __LINE__); exit(0);
   free(input);
 
   return 0;
+}
+
+void push(char str[], char data, int line)
+{
+   if (top == MAX_STRING-1 ){
+       printf("stack overflow at %d!\n", line); exit(0);
+   }
+   else{
+       top++;
+       str[top] = data;
+   }
+}
+
+int pop(char str[], int line)
+{
+   if (top == -1 ){
+       printf("stack underflow at %d!\n", line); return -1;
+   }
+   else{
+       return str[top];
+       top--;
+   }
 }
