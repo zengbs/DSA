3baec8067d564e3ed7daa646f69aa01f04917942
Po-Hsun Tseng <zengbs@gmail.com>
2021-04-29 00:23:55 +0800

    udpate

--- hw2/P5/go.sh
+++ hw2/P5/go.sh
@@ -2,5 +2,5 @@ export PATH=/software/gcc/9.3.0/bin:$PATH
 export LD_LIBRARY_PATH=/software/gcc/9.3.0/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/software/gcc/9.3.0/lib64:$LD_LIBRARY_PATH
 
-gcc main.c -g -Wall
+gcc main.c -g -Wall -O2
 ./a.out
--- hw2/P5/main.c
+++ hw2/P5/main.c
@@ -9,9 +9,8 @@
 #include<limits.h>
 #include "generator.h"
 
-typedef unsigned long long uint64_t;
 
-//#define JUDGE
+#define JUDGE
 
 struct PQArray{
     int p;
@@ -20,7 +19,9 @@ struct PQArray{
 };
 
 int compare(const void *struct1, const void *struct2) {
-    return ((struct PQArray *)struct1)->p - ((struct PQArray *)struct2)->p;
+      if ( ((struct PQArray *)struct1)->p <  ((struct PQArray *)struct2)->p ) return -1;
+      if ( ((struct PQArray *)struct1)->p == ((struct PQArray *)struct2)->p ) return 0;
+      if ( ((struct PQArray *)struct1)->p >  ((struct PQArray *)struct2)->p ) return 1;
 }
 
 
@@ -31,9 +32,9 @@ void swap(int *a, int *b)
   *a = *a - *b;
 }
 
-int Sum(int biTree[], int idx)
+long Sum(long biTree[], long idx)
 {
-	int sum = 0;
+	long sum = 0;
 
 	while (idx > 0)
 	{
@@ -44,7 +45,7 @@ int Sum(int biTree[], int idx)
 	return sum;
 }
 
-void Update(int biTree[], int n, int idx, int data)
+void Update(long biTree[], long n, long idx, long data)
 {
 	while (idx <= n)
 	{
@@ -54,11 +55,11 @@ void Update(int biTree[], int n, int idx, int data)
 	}
 }
 
-int InversionCount(int Q[], int R[], int left, int right, int Bit[], int bitSize)
+long InversionCount(int Q[], int R[], long left, long right, long Bit[], long bitSize)
 {
-	int c = 0;
+	long c = 0;
 
-	for (int i=right; i>=left; i--){
+	for (long i=right; i>=left; i--){
 		c += Sum(Bit, Q[i]);
 		Update(Bit, bitSize, R[i], 1);
 	}
@@ -70,9 +71,9 @@ int main()
 {
 #   ifndef JUDGE
     int numTriangle = 8;
-    int *P = (int *)malloc((size_t)numTriangle*sizeof(int));
-    int *Q = (int *)malloc((size_t)numTriangle*sizeof(int));
-    int *R = (int *)malloc((size_t)numTriangle*sizeof(int));
+    int *P = (int *)malloc((size_t)numTriangle*sizeof(long));
+    int *Q = (int *)malloc((size_t)numTriangle*sizeof(long));
+    int *R = (int *)malloc((size_t)numTriangle*sizeof(long));
 #   endif
 
 #   ifdef JUDGE
@@ -118,7 +119,7 @@ int main()
       R[7] = 0;
 #     endif
       
-      for (int i=0;i<numTriangle;i++){
+      for (long i=0;i<numTriangle;i++){
         pqrArray[i].p = P[i];
         pqrArray[i].q = Q[i];
         pqrArray[i].r = R[i];
@@ -128,7 +129,7 @@ int main()
       qsort(pqrArray, numTriangle, sizeof(*pqrArray), compare);
 
 
-      for (int i=0;i<numTriangle;i++){
+      for (long i=0;i<numTriangle;i++){
         P[i] = pqrArray[i].p;
         Q[i] = pqrArray[i].q;
         R[i] = pqrArray[i].r;
@@ -136,17 +137,17 @@ int main()
 
       free(pqrArray);
 
-//      for (int i=0;i<numTriangle;i++){
+//      for (long i=0;i<numTriangle;i++){
 //        printf("%2d ", pqrArray[i].p);
 //      }
 //      printf("\n"); 
 //
-//      for (int i=0;i<numTriangle;i++){
+//      for (long i=0;i<numTriangle;i++){
 //        printf("%2d ", pqrArray[i].q);
 //      }
 //      printf("\n"); 
 //
-//      for (int i=0;i<numTriangle;i++){
+//      for (long i=0;i<numTriangle;i++){
 //        printf("%2d ", pqrArray[i].r);
 //      }
 //
@@ -154,10 +155,10 @@ int main()
 //      exit(0);
 
       /* get the minimum item in P[] */
-      int minP = INT_MAX;
-      int minQ = INT_MAX;
-      int minR = INT_MAX;
-      for ( int i=0;i<numTriangle;i++ ){
+      long minP = LONG_MAX;
+      long minQ = LONG_MAX;
+      long minR = LONG_MAX;
+      for ( long i=0;i<numTriangle;i++ ){
         if ( minP > P[i] ) minP = P[i];
         if ( minQ > Q[i] ) minQ = Q[i];
         if ( minR > R[i] ) minR = R[i];
@@ -167,16 +168,16 @@ int main()
 
 
       /* get the minimum item in RPQ */
-      int minPQR[3] = {minP, minQ, minR};
-      int min = INT_MAX;
-      for (int i=0;i<3;i++){
+      long minPQR[3] = {minP, minQ, minR};
+      long min = LONG_MAX;
+      for (long i=0;i<3;i++){
         if (min > minPQR[i] ) min = minPQR[i];
       }
 
       min--;
 
       /* coordinate shift */
-      for (int i=0;i<numTriangle;i++){
+      for (long i=0;i<numTriangle;i++){
         P[i] -= min;
         Q[i] -= min;
         R[i] -= min;
@@ -184,29 +185,29 @@ int main()
 
 
       /* get the maximum item in Q[] */
-      int bitSize = 1;
+      long bitSize = 1;
 
-	  for (int i=0; i<numTriangle; i++){
+	  for (long i=0; i<numTriangle; i++){
         if (bitSize < Q[i]) bitSize = Q[i];
       }
 
       bitSize++;
    
-      int *Bit = (int *)calloc((size_t)bitSize, sizeof(int));
+      long *Bit = (long *)calloc((size_t)bitSize, sizeof(long));
 
-      int ans = InversionCount(Q, R, 0, numTriangle-1, Bit, bitSize);
+      long ans = InversionCount(Q, R, 0, numTriangle-1, Bit, bitSize);
 
       free(Bit);
 
-      int numDuplicate = 1;
-      int Temp = 0;
-      int partialCountTotal=0;
+      long numDuplicate = 1;
+      long Temp = 0;
+      long partialCountTotal=0;
 
-      for (int i=0;i<numTriangle;i=i+numDuplicate){
+      for (long i=0;i<numTriangle;i=i+numDuplicate){
 
         numDuplicate = 1;
 
-        int ii = i+1;
+        long ii = i+1;
 
         while( P[i] == P[ii] ){
 
@@ -218,12 +219,12 @@ int main()
         if (numDuplicate > 1){
           //printf("i=%d, i+numDuplicate-1=%d, numDuplicate=%d\n", i, i+numDuplicate-1, numDuplicate);
           bitSize = 1;
-	      for (int ii=i; ii<=i+numDuplicate-1; ii++){
+	      for (long ii=i; ii<=i+numDuplicate-1; ii++){
             if (bitSize < Q[ii]) bitSize = Q[ii];
           }
           bitSize++;
           //printf("bitSize=%d\n", bitSize);
-          Bit = (int *)calloc((size_t)bitSize, sizeof(int));
+          Bit = (long *)calloc((size_t)bitSize, sizeof(long));
 
           partialCountTotal += InversionCount(Q, R, i, i+numDuplicate-1, Bit, bitSize);
 
@@ -232,7 +233,7 @@ int main()
 
       }
    
-	  printf("%d\n",  ans-partialCountTotal+Temp);
+	  printf("%ld\n",  ans-partialCountTotal+Temp);
 #   ifdef JUDGE
     }
 #   endif
