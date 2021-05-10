6bc94150ad55b4a33d5878cf1be629ca92fedc5c
Po-Hsun Tseng <zengbs@gmail.com>
2021-05-10 17:12:01 +0800

    update

--- hw2/P6/main.c
+++ hw2/P6/main.c
@@ -10,7 +10,7 @@
 
 typedef struct Node_t {
 	struct Node_t* 	parent;
-	struct Node_t* 	next;
+	struct Node_t* 	sibling;
 	struct Node_t* 	child;
 
 	unsigned int 	degree;
@@ -25,29 +25,28 @@ typedef struct BHeap_t {
 }BHeap;
 
 
-void iheap_init(BHeap* heap)
+void init(BHeap* heap)
 {
 	heap->head = NULL;
 	heap->min  = NULL;
 }
 
-void iheap_node_init_ref(Node** _h,
-				      int key, const void* value)
+void node_init_ref(Node** heap, int key, const void* value)
 {
-	Node* h = *_h;
+	Node* h   = *heap;
 	h->parent = NULL;
-	h->next   = NULL;
+	h->sibling   = NULL;
 	h->child  = NULL;
 	h->degree = INT_MAX;
 	h->value  = value;
-	h->ref    = _h;
+	h->ref    = heap;
 	h->key    = key;
 }
 
-void iheap_node_init(Node* h, int key, const void* value)
+void node_init(Node* h, int key, const void* value)
 {
 	h->parent = NULL;
-	h->next   = NULL;
+	h->sibling   = NULL;
 	h->child  = NULL;
 	h->degree = INT_MAX;
 	h->value  = value;
@@ -55,208 +54,198 @@ void iheap_node_init(Node* h, int key, const void* value)
 	h->key    = key;
 }
 
- const void* iheap_node_value(Node* h)
+const void* node_value(Node* h)
 {
 	return h->value;
 }
 
- int iheap_node_in_heap(Node* h)
+int node_in_heap(Node* h)
 {
 	return h->degree != INT_MAX;
 }
 
- int iheap_empty(BHeap* heap)
+int empty(BHeap* heap)
 {
 	return heap->head == NULL && heap->min == NULL;
 }
 
-/* make child a subtree of root */
-void __iheap_link(Node* root,
+void heapLink(Node* root,
 			       Node* child)
 {
 	child->parent = root;
-	child->next   = root->child;
+	child->sibling   = root->child;
 	root->child   = child;
 	root->degree++;
 }
 
-/* merge root lists */
- Node* __iheap_merge(Node* a,
-					     Node* b)
+Node* heapMerge(Node* a, Node* b)
 {
 	Node* head = NULL;
 	Node** pos = &head;
 
-	while (a && b) {
-		if (a->degree < b->degree) {
-			*pos = a;
-			a = a->next;
-		} else {
-			*pos = b;
-			b = b->next;
-		}
-		pos = &(*pos)->next;
+	while (a && b){
+	  if (a->degree < b->degree){
+	  	*pos = a;
+	  	a = a->sibling;
+	  } else {
+	  	*pos = b;
+	  	b = b->sibling;
+	  }
+	  pos = &(*pos)->sibling;
 	}
-	if (a)
-		*pos = a;
-	else
-		*pos = b;
+
+	if (a) *pos = a;
+	else   *pos = b;
+
 	return head;
 }
 
-/* reverse a linked list of nodes. also clears parent pointer */
- Node* __iheap_reverse(Node* h)
+Node* heapReverse(Node* h)
 {
 	Node* tail = NULL;
-	Node* next;
+	Node* sibling;
 
-	if (!h)
-		return h;
+	if (h != NULL) return h;
 
 	h->parent = NULL;
-	while (h->next) {
-		next    = h->next;
-		h->next = tail;
-		tail    = h;
-		h       = next;
-		h->parent = NULL;
+
+	while (h->sibling){
+	  sibling    = h->sibling;
+	  h->sibling = tail;
+	  tail       = h;
+	  h          = sibling;
+	  h->parent  = NULL;
 	}
-	h->next = tail;
+
+	h->sibling = tail;
+
 	return h;
 }
 
-void __iheap_min(BHeap* heap,
-			      Node** prev,
-			      Node** node)
+void getMinNode(BHeap* heap, Node** prev, Node** node)
 {
-	Node *_prev, *cur;
+	Node *nodePrev, *cur;
 	*prev = NULL;
 
-	if (!heap->head) {
+	if (!heap->head){
 		*node = NULL;
 		return;
 	}
 
 	*node = heap->head;
-	_prev = heap->head;
-	cur   = heap->head->next;
-	while (cur) {
-		if (cur->key < (*node)->key) {
-			*node = cur;
-			*prev = _prev;
-		}
-		_prev = cur;
-		cur   = cur->next;
+	nodePrev = heap->head;
+	cur   = heap->head->sibling;
+
+	while (cur){
+
+	  if (cur->key < (*node)->key){
+	  	*node = cur;
+	  	*prev = nodePrev;
+	  }
+
+	  nodePrev = cur;
+	  cur   = cur->sibling;
 	}
 }
 
-void __iheap_union(BHeap* heap, Node* h2)
+void heapUnion(BHeap* heap, Node* h2)
 {
 	Node* h1;
-	Node *prev, *x, *next;
-	if (!h2)
-		return;
+	Node *prev, *x, *sibling;
+
+	if (!h2) return;
+
 	h1 = heap->head;
-	if (!h1) {
-		heap->head = h2;
-		return;
+
+	if (!h1){
+      heap->head = h2;
+      return;
 	}
-	h1 = __iheap_merge(h1, h2);
-	prev = NULL;
-	x    = h1;
-	next = x->next;
-	while (next) {
-		if (x->degree != next->degree ||
-		    (next->next && next->next->degree == x->degree)) {
-			/* nothing to do, advance */
-			prev = x;
-			x    = next;
-		} else if (x->key < next->key) {
-			/* x becomes the root of next */
-			x->next = next->next;
-			__iheap_link(x, next);
-		} else {
-			/* next becomes the root of x */
-			if (prev)
-				prev->next = next;
-			else
-				h1 = next;
-			__iheap_link(next, x);
-			x = next;
-		}
-		next = x->next;
+
+	h1      = heapMerge(h1, h2);
+	prev    = NULL;
+	x       = h1;
+	sibling = x->sibling;
+
+	while (sibling){
+	  if (x->degree != sibling->degree || (sibling->sibling && sibling->sibling->degree == x->degree)){
+	  	prev = x;
+	  	x    = sibling;
+	  } else if (x->key < sibling->key){
+	  	x->sibling = sibling->sibling;
+	  	heapLink(x, sibling);
+	  } else {
+	  	if (prev) prev->sibling = sibling;
+	  	else      h1 = sibling;
+
+	  	heapLink(sibling, x);
+	  	x = sibling;
+	  }
+	  sibling = x->sibling;
 	}
+
 	heap->head = h1;
 }
 
- Node* __iheap_extract_min(BHeap* heap)
+Node* extractMin(BHeap* heap)
 {
 	Node *prev, *node;
-	__iheap_min(heap, &prev, &node);
-	if (!node)
-		return NULL;
-	if (prev)
-		prev->next = node->next;
-	else
-		heap->head = node->next;
-	__iheap_union(heap, __iheap_reverse(node->child));
+	getMinNode(heap, &prev, &node);
+
+	if (!node) return NULL;
+
+	if (prev) prev->sibling = node->sibling;
+	else      heap->head = node->sibling;
+
+	heapUnion(heap, heapReverse(node->child));
+
 	return node;
 }
 
-/* insert (and reinitialize) a node into the heap */
-void iheap_insert(BHeap* heap, Node* node)
+void insert(BHeap* heap, Node* node)
 {
 	Node *min;
-	node->child  = NULL;
-	node->parent = NULL;
-	node->next   = NULL;
-	node->degree = 0;
-	if (heap->min && node->key < heap->min->key) {
-		/* swap min cache */
-		min = heap->min;
-		min->child  = NULL;
-		min->parent = NULL;
-		min->next   = NULL;
-		min->degree = 0;
-		__iheap_union(heap, min);
-		heap->min   = node;
-	} else
-		__iheap_union(heap, node);
+	node->child     = NULL;
+	node->parent    = NULL;
+	node->sibling   = NULL;
+	node->degree    = 0;
+
+	if (heap->min && node->key < heap->min->key){
+
+	  min = heap->min;
+	  min->child     = NULL;
+	  min->parent    = NULL;
+	  min->sibling   = NULL;
+	  min->degree    = 0;
+	  heapUnion(heap, min);
+	  heap->min      = node;
+
+	} else heapUnion(heap, node);
 }
 
-void __iheap_uncache_min(BHeap* heap)
+void uncacheMin(BHeap* heap)
 {
 	Node* min;
-	if (heap->min) {
+	if (heap->min){
 		min = heap->min;
 		heap->min = NULL;
-		iheap_insert(heap, min);
+		insert(heap, min);
 	}
 }
 
-/* merge addition into target */
-void iheap_union(BHeap* target, BHeap* addition)
-{
-	/* first insert any cached minima, if necessary */
-	__iheap_uncache_min(target);
-	__iheap_uncache_min(addition);
-	__iheap_union(target, addition->head);
-	/* this is a destructive merge */
-	addition->head = NULL;
-}
 
- Node* iheap_peek(BHeap* heap)
+Node* peek(BHeap* heap)
 {
 	if (!heap->min)
-		heap->min = __iheap_extract_min(heap);
+		heap->min = extractMin(heap);
 	return heap->min;
 }
 
- Node* iheap_take(BHeap* heap)
+Node* take(BHeap* heap)
 {
 	Node *node;
 	if (!heap->min)
-		heap->min = __iheap_extract_min(heap);
+		heap->min = extractMin(heap);
 	node = heap->min;
 	heap->min = NULL;
 	if (node)
@@ -264,92 +253,87 @@ void iheap_union(BHeap* target, BHeap* addition)
 	return node;
 }
 
-void iheap_decrease(BHeap* heap, Node* node,
-				  int new_key)
+void decrease(BHeap* heap, Node* node, int new_key)
 {
 	Node *parent;
 	Node** tmp_ref;
 	const void* tmp;
 	int   tmp_key;
 
-	/* node's priority was decreased, we need to update its position */
-	if (!node->ref || new_key >= node->key)
-		return;
+	if (!node->ref || new_key >= node->key) return;
+
 	node->key = new_key;
-	if (heap->min != node) {
+
+	if (heap->min != node){
+
 		if (heap->min && node->key < heap->min->key)
-			__iheap_uncache_min(heap);
-		/* bubble up */
+			uncacheMin(heap);
 		parent = node->parent;
-		while (parent && node->key < parent->key) {
-			/* swap parent and node */
+
+		while (parent && node->key < parent->key){
 			tmp           = parent->value;
 			tmp_key       = parent->key;
 			parent->value = node->value;
 			parent->key   = node->key;
 			node->value   = tmp;
 			node->key     = tmp_key;
-			/* swap references */
 			if (parent->ref)
 				*(parent->ref) = node;
 			*(node->ref)   = parent;
 			tmp_ref        = parent->ref;
 			parent->ref    = node->ref;
 			node->ref      = tmp_ref;
-			/* step up */
 			node   = parent;
 			parent = node->parent;
 		}
 	}
 }
 
-void iheap_delete(BHeap* heap, Node* node)
+void delete(BHeap* heap, Node* node)
 {
 	Node *parent, *prev, *pos;
 	Node** tmp_ref;
 	const void* tmp;
 	int tmp_key;
 
-	if (!node->ref) /* can only delete if we have a reference */
-		return;
-	if (heap->min != node) {
-		/* bubble up */
-		parent = node->parent;
-		while (parent) {
-			/* swap parent and node */
-			tmp           = parent->value;
-			tmp_key       = parent->key;
-			parent->value = node->value;
-			parent->key   = node->key;
-			node->value   = tmp;
-			node->key     = tmp_key;
-			/* swap references */
-			if (parent->ref)
-				*(parent->ref) = node;
-			*(node->ref)   = parent;
-			tmp_ref        = parent->ref;
-			parent->ref    = node->ref;
-			node->ref      = tmp_ref;
-			/* step up */
-			node   = parent;
-			parent = node->parent;
-		}
-		/* now delete:
-		 * first find prev */
-		prev = NULL;
-		pos  = heap->head;
-		while (pos != node) {
-			prev = pos;
-			pos  = pos->next;
-		}
-		/* we have prev, now remove node */
-		if (prev)
-			prev->next = node->next;
-		else
-			heap->head = node->next;
-		__iheap_union(heap, __iheap_reverse(node->child));
-	} else
-		heap->min = NULL;
+	if (!node->ref) return;
+
+	if (heap->min != node){
+	  parent = node->parent;
+	  while (parent){
+	  	tmp           = parent->value;
+	  	tmp_key       = parent->key;
+	  	parent->value = node->value;
+	  	parent->key   = node->key;
+	  	node->value   = tmp;
+	  	node->key     = tmp_key;
+
+	  	if (parent->ref) *(parent->ref) = node;
+
+	  	*(node->ref)   = parent;
+	  	tmp_ref        = parent->ref;
+	  	parent->ref    = node->ref;
+	  	node->ref      = tmp_ref;
+	  	node   = parent;
+	  	parent = node->parent;
+	  }
+
+	  prev = NULL;
+	  pos  = heap->head;
+
+	  while (pos != node){
+	  	prev = pos;
+	  	pos  = pos->sibling;
+	  }
+
+	  if (prev) prev->sibling = node->sibling;
+	  else      heap->head    = node->sibling;
+
+	  heapUnion(heap, heapReverse(node->child));
+
+	} else heap->min = NULL;
+
+
 	node->degree = INT_MAX;
 }
 
