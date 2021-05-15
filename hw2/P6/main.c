1326910e1a677dc5cb7f5018f6b9e7ce4dd51fd2
Po-Hsun Tseng <zengbs@gmail.com>
2021-05-15 15:56:06 +0800

    update

--- hw2/P6/main.c
+++ hw2/P6/main.c
@@ -5,6 +5,8 @@
 #include<stdint.h>
 #include<limits.h>
 
+
+
 typedef struct Node_t {
 	struct Node_t* 	parent;
 	struct Node_t* 	sibling;
@@ -12,318 +14,73 @@ typedef struct Node_t {
 
 	unsigned int 	degree;
 	int			    key;
-	const void*	   	value;
-	struct Node_t**	ref;
 }Node;
 
 typedef struct BHeap_t {
 	struct Node_t* 	head;
-	struct Node_t*	min;
 }BHeap;
 
+void SwapInt(int *x, int *y);
+void heapInit(BHeap* heap);  
+void heapNodeInit(Node* node, int key);  
 
-void init(BHeap* heap)
+
+void SwapInt(int *x, int *y)
+{
+    *x = (*x) ^ (*y);
+    *y = (*x) ^ (*y);
+    *x = (*x) ^ (*y);
+}
+
+void heapInit(BHeap *heap)
 {
 	heap->head = NULL;
-	heap->min  = NULL;
 }
 
-void node_init_ref(Node** heap, int key, const void* value)
+void heapNodeInit(Node* node, int key)
 {
-	Node* h     = *heap;
-	h->parent   = NULL;
-	h->sibling  = NULL;
-	h->child    = NULL;
-	h->degree   = INT_MAX;
-	h->value    = value;
-	h->ref      = heap;
-	h->key      = key;
+	node->parent   = NULL;
+	node->sibling  = NULL;
+	node->child    = NULL;
+	node->degree   = INT_MAX;
+	node->key      = key;
 }
 
-void node_init(Node* h, int key, const void* value)
+
+Node* heapMin(BHeap *heap)
 {
-	h->parent   = NULL;
-	h->sibling  = NULL;
-	h->child    = NULL;
-	h->degree   = INT_MAX;
-	h->value    = value;
-	h->ref      = NULL;
-	h->key      = key;
+  Node *root = heap->head;
+  Node *curNode = NULL;
+
+  int min = INT_MAX;
+
+  while( curNode ){
+    if ( min > root->key ){
+      min     = root->key;
+      curNode = root;
+      root    = root->sibling;
+    }
+  }
+
+  return curNode;
 }
 
 
-
-void heapLink(Node* root, Node* child)
+/* Links binomial trees whose roots have the same degree */
+void heapLink(Node* root1, Node* root2)
 {
-	child->parent = root;
-	child->sibling   = root->child;
-	root->child   = child;
-	root->degree++;
-}
-
-Node* heapMerge(Node* a, Node* b)
-{
-	Node* head = NULL;
-	Node** pos = &head;
-
-	while (a && b){
-	  if (a->degree < b->degree){
-	  	*pos = a;
-	  	a = a->sibling;
-	  } else {
-	  	*pos = b;
-	  	b = b->sibling;
-	  }
-	  pos = &(*pos)->sibling;
-	}
-
-	if (a) *pos = a;
-	else   *pos = b;
-
-	return head;
-}
-
-Node* heapReverse(Node* h)
-{
-	Node* tail = NULL;
-	Node* sibling;
-
-	if (h != NULL) return h;
-
-	h->parent = NULL;
-
-	while (h->sibling){
-	  sibling    = h->sibling;
-	  h->sibling = tail;
-	  tail       = h;
-	  h          = sibling;
-	  h->parent  = NULL;
-	}
-
-	h->sibling = tail;
-
-	return h;
-}
-
-void getMinNode(BHeap* heap, Node** prev, Node** node)
-{
-	Node *nodePrev, *nodeCur;
-	*prev = NULL;
-
-	if (!heap->head){
-		*node = NULL;
-		return;
-	}
-
-	*node = heap->head;
-	nodePrev = heap->head;
-	nodeCur   = heap->head->sibling;
-
-	while (nodeCur){
-
-	  if (nodeCur->key < (*node)->key){
-	  	*node = nodeCur;
-	  	*prev = nodePrev;
-	  }
-
-	  nodePrev = nodeCur;
-	  nodeCur   = nodeCur->sibling;
-	}
-}
-
-void heapUnion(BHeap* heap, Node* h2)
-{
-	Node* h1;
-	Node *prev, *x, *sibling;
-
-	if (!h2) return;
-
-	h1 = heap->head;
-
-	if (!h1){
-      heap->head = h2;
-      return;
-	}
-
-	h1      = heapMerge(h1, h2);
-	prev    = NULL;
-	x       = h1;
-	sibling = x->sibling;
-
-	while (sibling){
-	  if (x->degree != sibling->degree || (sibling->sibling && sibling->sibling->degree == x->degree)){
-	  	prev = x;
-	  	x    = sibling;
-	  } else if (x->key < sibling->key){
-	  	x->sibling = sibling->sibling;
-	  	heapLink(x, sibling);
-	  } else {
-
-	  	if (prev) prev->sibling = sibling;
-	  	else      h1 = sibling;
-
-	  	heapLink(sibling, x);
-	  	x = sibling;
-	  }
-	  sibling = x->sibling;
-	}
-
-	heap->head = h1;
-}
-
-Node* extractMin(BHeap* heap)
-{
-	Node *prev, *node;
-
-	getMinNode(heap, &prev, &node);
-
-	if (!node) return NULL;
-
-	if (prev) prev->sibling = node->sibling;
-	else      heap->head = node->sibling;
-
-	heapUnion(heap, heapReverse(node->child));
-
-	return node;
-}
-
-void insert(BHeap* heap, Node* node)
-{
-	Node *min;
-	node->child     = NULL;
-	node->parent    = NULL;
-	node->sibling   = NULL;
-	node->degree    = 0;
-
-	if (heap->min && node->key < heap->min->key){
-
-	  min = heap->min;
-	  min->child     = NULL;
-	  min->parent    = NULL;
-	  min->sibling   = NULL;
-	  min->degree    = 0;
-	  heapUnion(heap, min);
-	  heap->min      = node;
-
-	} else heapUnion(heap, node);
-}
-
-void uncacheMin(BHeap* heap)
-{
-	Node* min;
-	if (heap->min){
-		min = heap->min;
-		heap->min = NULL;
-		insert(heap, min);
-	}
-}
-
-
-Node* peek(BHeap* heap)
-{
-	if (!heap->min)
-		heap->min = extractMin(heap);
-	return heap->min;
-}
-
-Node* take(BHeap* heap)
-{
-	Node *node;
-
-	if (!heap->min) heap->min = extractMin(heap);
-
-	node = heap->min;
-	heap->min = NULL;
-
-	if (node) node->degree = INT_MAX;
-
-	return node;
-}
-
-void decrease(BHeap* heap, Node* node, int new_key)
-{
-	Node *parent;
-	Node** tmp_ref;
-	const void* tmp;
-	int   tmp_key;
-
-	if (!node->ref || new_key >= node->key) return;
-
-	node->key = new_key;
-
-	if (heap->min != node){
-
-		if (heap->min && node->key < heap->min->key) uncacheMin(heap);
-
-		parent = node->parent;
-
-		while (parent && node->key < parent->key){
-			tmp           = parent->value;
-			tmp_key       = parent->key;
-			parent->value = node->value;
-			parent->key   = node->key;
-			node->value   = tmp;
-			node->key     = tmp_key;
-
-			if (parent->ref) *(parent->ref) = node;
-
-			*(node->ref)   = parent;
-			tmp_ref        = parent->ref;
-			parent->ref    = node->ref;
-			node->ref      = tmp_ref;
-			node           = parent;
-			parent         = node->parent;
-		}
-	}
-}
-
-void delete(BHeap* heap, Node* node)
-{
-	Node *parent, *prev, *pos;
-	Node** tmp_ref;
-	const void* tmp;
-	int tmp_key;
-
-	if (!node->ref) return;
-
-	if (heap->min != node){
-	  parent = node->parent;
-
-	  while (parent){
-	  	tmp           = parent->value;
-	  	tmp_key       = parent->key;
-	  	parent->value = node->value;
-	  	parent->key   = node->key;
-	  	node->value   = tmp;
-	  	node->key     = tmp_key;
-
-	  	if (parent->ref) *(parent->ref) = node;
-
-	  	*(node->ref)   = parent;
-	  	tmp_ref        = parent->ref;
-	  	parent->ref    = node->ref;
-	  	node->ref      = tmp_ref;
-	  	node   = parent;
-	  	parent = node->parent;
-	  }
-
-	  prev = NULL;
-	  pos  = heap->head;
-
-	  while (pos != node){
-	  	prev = pos;
-	  	pos  = pos->sibling;
-	  }
-
-	  if (prev) prev->sibling = node->sibling;
-	  else      heap->head    = node->sibling;
-
-	  heapUnion(heap, heapReverse(node->child));
-
-	} else heap->min = NULL;
-
-
-	node->degree = INT_MAX;
+  if (root1->key > root2->key){
+     root1->parent = root2;
+     root1->sibling = root2->child;
+     root2->child = root1;
+     root2->degree++
+  }
+  else{
+     root2->parent = root1;
+     root2->sibling = root1->child;
+     root1->child = root2;
+     root1->degree++
+  }
 }
 
 
@@ -331,99 +88,338 @@ void delete(BHeap* heap, Node* node)
 
 
 
-/* Deque */
-struct Deque{
+
+
+
+/*============================ Deque ===================================*/
+typedef struct Deque_t{
   int data;
-  struct Deque *npx;
-};
+  struct Deque_t *npx;
+}Deque;
 
 
-struct Deque* XOR(struct Deque *a, struct Deque *b)
+Deque* XOR(Deque *a, Deque *b)
 {
-  return (struct Deque*)((uintptr_t) (a) ^ (uintptr_t) (b));
+  return (Deque*)((uintptr_t) (a) ^ (uintptr_t) (b));
 }
 
 
-void printList (struct Deque *endPoint)
+void printDequeRight (Deque *rightPoint)
 {
-    struct Deque *curr = endPoint;
+    Deque *curr = rightPoint;
 
-    struct Deque *prev = NULL;
-    struct Deque *sibling;
+    Deque *prev = NULL;
+    Deque *next;
+
 
     while (curr != NULL)
     {
         printf ("%d ", curr->data);
-        sibling = XOR (prev, curr->npx);
+        next = XOR (prev, curr->npx);
         prev = curr;
-        curr = sibling;
+        curr = next;
     }
   printf("\n");
 }
 
-
-
-void push(struct Deque **endPoint, int data)
+void printDequeLeft (Deque *leftPoint)
 {
-  struct Deque *newNode = (struct Deque *) malloc (sizeof (struct Deque) );
-  newNode->data = data;
-  newNode->npx = *endPoint;
+   Deque *curr = leftPoint;
 
-  if (*endPoint != NULL)
-  {
-      (*endPoint)->npx = XOR(newNode, (*endPoint)->npx);
-  }
+   Deque *prev = NULL;
+   Deque *next;
 
-  *endPoint = newNode;
+   while (curr != NULL)
+   {
+       printf ("%d ", curr->data);
+       next = XOR (prev, curr->npx);
+       prev = curr;
+       curr = next;
+   }
+
+   printf("\n");
 }
 
 
-void pop(struct Deque **endPoint)
+void pushDeque(Deque **leftPoint, Deque **rightPoint, int data)
 {
-  struct Deque *siblingNode;
-  struct Deque *siblingsiblingNode;
+  Deque *newNode = (Deque *) malloc (sizeof (Deque) );
+  newNode->data = data;
 
-  siblingNode     = (*endPoint)->npx;
 
-  if (siblingNode != NULL){
-
-    siblingsiblingNode = XOR(siblingNode->npx, *endPoint);
-    siblingNode->npx  = siblingsiblingNode;
-
-    free(*endPoint);
-    *endPoint = siblingNode;
+  if (*rightPoint != NULL)
+  {
+    Deque *rightSecondPoint = XOR( (*rightPoint)->npx, NULL );
+    newNode->npx = XOR(*rightPoint, NULL);
+    (*rightPoint)->npx = XOR(rightSecondPoint, newNode);
   }
   else{
-    free(*endPoint);
-    *endPoint = NULL;
+    newNode->npx = XOR(NULL, NULL);
+    *leftPoint = newNode;
   }
+
+  *rightPoint = newNode;
 }
 
 
+void popRightDeque(Deque **rightPoint)
+{
+  Deque *nextNode;
+  Deque *nextnextNode;
+
+  nextNode     = (*rightPoint)->npx;
+
+  if (nextNode != NULL){
+
+    nextnextNode = XOR(nextNode->npx, *rightPoint);
+    nextNode->npx  = nextnextNode;
+
+    free(*rightPoint);
+    *rightPoint = nextNode;
+  }
+  else{
+    free(*rightPoint);
+    *rightPoint = NULL;
+  }
+}
+
+void popLeftDeque(Deque **leftPoint)
+{
+  Deque *nextNode;
+  Deque *nextnextNode;
+
+  nextNode     = (*leftPoint)->npx;
+
+  if (nextNode != NULL){
+
+    nextnextNode = XOR(nextNode->npx, *leftPoint);
+    nextNode->npx  = nextnextNode;
+
+    free(*leftPoint);
+    *leftPoint = nextNode;
+  }
+  else{
+    free(*leftPoint);
+    *leftPoint = NULL;
+  }
+}
+
+void mergeDeque(Deque **leftPoint_running, Deque **rightPoint_running,
+                Deque **leftPoint_broken,  Deque **rightPoint_broken )
+{
+    if (*rightPoint_running != NULL && *leftPoint_broken != NULL){
+      Deque *secondRightPoint_running = XOR( (*rightPoint_running)->npx, NULL );
+      (*rightPoint_running)->npx      = XOR( secondRightPoint_running, *leftPoint_broken );
+
+      Deque *secondLeftPoint_broken = XOR( NULL, (*leftPoint_broken)->npx );
+      (*leftPoint_broken)->npx      = XOR( *rightPoint_running, secondLeftPoint_broken );
+
+      *rightPoint_running = *rightPoint_broken;
+      *leftPoint_broken  = NULL;
+      *rightPoint_broken = NULL;
+    }
+    else if ( *rightPoint_running == NULL && *leftPoint_broken != NULL ) {
+      Deque *secondRightPoint_broken = XOR( (*rightPoint_broken)->npx, NULL );
+      Deque *secondLeftPoint_broken = XOR( NULL, (*leftPoint_broken)->npx );
+
+      *leftPoint_running  = *leftPoint_broken;
+      *rightPoint_running = *rightPoint_broken;
+
+      (*leftPoint_running)->npx  = XOR( secondLeftPoint_broken,  NULL );
+      (*rightPoint_running)->npx = XOR( secondRightPoint_broken, NULL );
+
+      *rightPoint_running = *rightPoint_broken;
+      *leftPoint_running  = *leftPoint_broken;
+      *leftPoint_broken = NULL;
+      *rightPoint_broken = NULL;
+    }
+}
 
 int main(){
 
+  //Deque *left  = NULL;
+  //Deque *right = NULL;
 
-//  struct Deque *left  = NULL;
-//  struct Deque *right = NULL;
-//
-//  push(&left, 1);
-//
-//  right = left;
-//
-//  push(&left, 2);
-//  push(&left, 3);
-//  push(&right, 99);
-//  push(&left, 4);
-//  pop(&left);
-//  push(&right, -1);
-//  push(&right, 0);
-//  pop(&right);
-//  push(&right, 55);
-//
-//
-//  printList(left);
+  //pushDeque(&left, &right,-1);
+  //pushDeque(&left, &right, 1);
+  //pushDeque(&left, &right, 2);
+  //pushDeque(&left, &right, 3);
+  //popRightDeque(&right);
+  //popRightDeque(&right);
+  //popRightDeque(&left);
+  //pushDeque(&left, &right, 8);
+  //pushDeque(&left, &right, 9);
+  //pushDeque(&left, &right, 4);
+  //pushDeque(&left, &right, 5);
+  //popLeftDeque(&right);
+  //popLeftDeque(&left);
+  //popLeftDeque(&left);
+  //pushDeque(&left, &right, 6);
 
+  //printDequeLeft(left);
+
+  //popRightDeque();
+
+
+
+  //exit(0);
+
+  /* number of test cases */
+  int Tsize;
+
+  /* number of  packages*/
+  int Nsize;
+
+  /* number of operations (merge and push)*/
+  int Osize;
+
+  /* number of production line*/
+  int Lsize;
+
+  scanf("%d", &Tsize);
+
+  /*operations*/
+  char operation[8];
+
+  int t=0;
+
+  while( t < Tsize ){
+
+     scanf("%d %d %d", &Nsize, &Osize, &Lsize);
+
+     /*store input parameters*/
+     int *operation_0    = (int*)malloc((size_t)Osize*sizeof(int));
+     int *operation_1    = (int*)malloc((size_t)Osize*sizeof(int));
+     int *operation_2    = (int*)malloc((size_t)Osize*sizeof(int));
+     int *packagesHeight = (int*)malloc((size_t)Nsize*sizeof(int));
+
+     int packageHeight, productionLine, brokenLine, runningLine;
+
+     for (int o=0;o<Osize;o++){
+
+       scanf("%7s", operation);
+
+       if      (strcmp(operation, "push")== 0){
+         /* read push parameters */
+         scanf("%d %d", &packageHeight, &productionLine);
+         operation_0[o] = 0;
+         operation_1[o] = packageHeight;
+         operation_2[o] = productionLine;
+       }
+
+       else if (strcmp(operation, "merge")== 0){
+         /* read merge parameters */
+         scanf("%d %d", &brokenLine, &runningLine);
+         operation_0[o] = 1;
+         operation_1[o] = brokenLine;
+         operation_2[o] = runningLine;
+       }
+     }
+
+     /* read packages height */
+     for (int n=0;n<Nsize;n++){
+       scanf("%d", &packagesHeight[n]);
+     }
+
+     /*==================================================*/
+     BHeap* heap = (BHeap*)malloc(sizeof(BHeap));
+     heapInit(heap);
+
+     /* allocate heap memory for heaps */
+     Node **root = (Node**)malloc((size_t)Lsize*sizeof(Node*));
+
+     heap->head = root[0];
+     heap->min  = NULL;
+     
+     Node *curRoot = root[0];
+
+     /* make linked list for heaps */
+     for (int i=0;i<Lsize;i++){
+
+       if (i==Lsize-1) curRoot->sibling = NULL;
+       else            curRoot->sibling = root[i+1];
+
+       curRoot = curRoot->sibling;
+     }
+
+     /* allocate deque memory for `L` production lines */
+     Deque **leftPoint   = (Deque**)malloc((size_t)(Lsize)*sizeof(Deque*));
+     Deque **rightPoint  = (Deque**)malloc((size_t)(Lsize)*sizeof(Deque*));
+
+     /* initialize deque pointer */
+     for (int i=0;i<Lsize;i++){
+       leftPoint[i]   = NULL;
+       rightPoint[i]  = NULL;
+     }
+
+
+
+     /*
+     * perform `push` or `merge` on deque or heaps
+     * and try to `pop out` from heap or deque
+     *  */
+     for (int o=0;o<Osize;o++){
+
+       /*=========== push =============*/
+       if (operation_0[o] == 0){
+         packageHeight  = operation_1[o];
+         productionLine = operation_2[o];
+
+         /* A. push `packageHeight` into deque */
+
+         pushDeque(&leftPoint[productionLine], &rightPoint[productionLine], packageHeight);
+
+
+         /* B. insert into heap */
+         //Node *node = malloc(sizeof(Node));
+         //HeapNodeInit(node, packageHeight);
+         //HeapInsert(root[productionLine], node);
+
+       }
+
+       /*=========== merge =============*/
+       else{
+         brokenLine     = operation_1[o];
+         runningLine    = operation_2[o];
+
+
+         /* merge deques */
+         mergeDeque(&leftPoint[runningLine], &rightPoint[runningLine],
+                    &leftPoint[brokenLine],  &rightPoint[brokenLine]);
+
+         /* union heap */
+         //heapUnion(root[runningLine], root[brokenLine]);
+       }
+
+     } // for (int o=0;o<Osize;o++)
+
+
+     /* ============ print deque ================== */
+     for(int i=0;i<Lsize;i++)   printDequeLeft(leftPoint[i]);
+
+     for(int i=0;i<Lsize;i++){
+       Node *minNode;
+
+       //minNode = heapExtractMin(heapLine[i]);
+       //heapGetMinNode(heapLine[i], &minNode);
+
+      // if (minNode && minNode->child){
+      //   //heapDecrease(heapLine[i],minNode->child, -1);
+      //   printf("%p\n", minNode->child);
+      // }
+       //minNode = heapPeekMin(heapLine[i]);
+
+     //  if ( minNode )  printf("Line=%d, min=%d, degree=%d, child=%p\n", i, minNode->key, minNode->degree, minNode->child);
+     }
+
+     t++;
+
+     free(operation_0);
+     free(operation_1);
+     free(operation_2);
+     free(packagesHeight);
+
+  }
 
   return 0;
 }
