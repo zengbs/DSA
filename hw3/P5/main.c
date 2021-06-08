d590eb1cd7afe51271cb47f3912754d30bfe6909
Po-Hsun Tseng <zengbs@gmail.com>
2021-06-08 16:02:45 +0800

    update

--- hw3/P5/main.c
+++ hw3/P5/main.c
@@ -5,24 +5,62 @@
 #include<stdint.h>
 #include<limits.h>
 
+#define MAX_BOOKS 100000
+
+void printPriority(int *sizePriorityArray, int *priorityArray)
+{
+  for (int i=0;i<*sizePriorityArray; i++){
+    printf("%d ", priorityArray[i]);
+  }
+  printf("\n");
+}
+
+
+void Insert(int p, int k, int *sizePriorityArray, int *priorityArray)
+{
+  if (k > *sizePriorityArray+1){
+    printf("k(%d) > sizePriorityArray(%d)\n", k, *sizePriorityArray);
+    exit(0);
+  }
+
+  for(int i=*sizePriorityArray ;i>k; i--){
+    priorityArray[i] = priorityArray[i-1];
+  }
+  
+  *sizePriorityArray = *sizePriorityArray + 1;
+  priorityArray[k] = p;
+}
+
+void Delete(int k, int *sizePriorityArray, int *priorityArray)
+{
+  for(int i=k ;i<*sizePriorityArray; i++){
+    priorityArray[i-1] = priorityArray[i];
+  }
+
+  *sizePriorityArray = *sizePriorityArray - 1;
+}
+
+
 int main(){
+
 
   /* number of books in initial bookshelf */
   int N;
+
 
   /* number of operations*/
   int Q;
 
   scanf("%d %d", &N, &Q);
 
+  int sizePriorityArray = N;
+
   /* priority array*/
-  int *priorityArray = (int*)calloc((size_t)N, sizeof(int));
+  int *priorityArray = (int*)calloc((size_t)MAX_BOOKS, sizeof(int));
 
   for(int i=0;i<N;i++){
     scanf("%d", &priorityArray[i]);
-    printf("%d ", priorityArray[i]);
   }
-  printf("\n");
 
   /*operations*/
   int operation;
@@ -38,6 +76,7 @@ int main(){
   /*position*/
   int k;
 
+ 
 
   switch(operation)
   {
@@ -45,13 +84,15 @@ int main(){
     /* insert */
       scanf("%d", &p);
       scanf("%d", &k);
-
+      Insert(p, k, &sizePriorityArray, priorityArray);
       break;
+
     case 2:
     /* delete */
       scanf("%d", &k);
-
+      Delete(k, &sizePriorityArray, priorityArray);
       break;
+
     case 3:
     /* increase priority */
       scanf("%d", &l);
@@ -78,5 +119,9 @@ int main(){
 
       break;
   }
+
+
+  printPriority(&sizePriorityArray, priorityArray);
+
   return 0;
 }
