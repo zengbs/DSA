d68e17f8012f73da10d70f03b59ba959b26431f3
Po-Hsun Tseng <zengbs@gmail.com>
2021-06-13 17:56:41 +0800

    update

--- hw3/P6/main.c
+++ hw3/P6/main.c
@@ -23,29 +23,74 @@ int main(){
   
   scanf("%d", &lengthAdjList);
 
-  int **head = (int**)malloc((size_t)(lengthAdjList)*sizeof(int*));
+  int **head   = (int**)malloc((size_t)(lengthAdjList)*sizeof(int*));
+  int  *numVex = (int* )malloc((size_t)(lengthAdjList)*sizeof(int ));
+  int **Ptr    = (int**)malloc((size_t)(lengthAdjList)*sizeof(int*));
 
   /*============== STEP-1: input ============== */
   for (int i=0;i<lengthAdjList;i++){
-    int lengthAdj;
-    scanf("%d", &lengthAdj);
+
+    scanf("%d", &numVex[i]);
  
-    head[i] = (int*)malloc((size_t)(lengthAdj+1)*sizeof(int));
+    head[i] = (int*)malloc((size_t)(&numVex[i])*sizeof(int));
 
-    head[i][0] = lengthAdj;
 
-    for (int j=1;j<lengthAdj+1;j++){
+    for (int j=0;j<numVex[i];j++){
       scanf("%d", &head[i][j]);
+      head[i][j]--;
     }
+
+    *Ptr[i] = &head[i][0];
   }
 
   /*============== STEP-2:  ============== */
+  int i = 0;
+  int j = 0;
+
+  int p1, p2;
+
+ for(int i=0;i<lengthAdjList;i++){
+
+      p1 = i;
+ 
+      while ( *Ptr[*Ptr[p1]] == p1 ){
+
+        p2 = *Ptr[p1];
+
+        printf("%d %d\n",p1, p2);
+
+        j++;
 
 
+
+        Ptr[p1] = &head[p1][j];
+        Ptr[p2] = &head[p2][j];
+
+
+        if ( *Ptr[*Ptr[p1]] == p1 ){
+
+          p2 = *Ptr[p1];
+
+        }else if ( *Ptr[*Ptr[p2]] == p2 ){
+
+          p1 = *Ptr[p2];
+
+        }else{
+          printf("error!");
+          exit(0);
+        }
+
+      }
+
+ }
+
+
+
+  /*============== STEP-999:  ============== */
   /* output */
   for (int i=0;i<lengthAdjList;i++){
-    for (int j=0;j<head[i][0]+1;j++){
-      printf("%d ", head[i][j]);
+    for (int j=0;j<numVex[i];j++){
+      printf("%d ", head[i][j]+1);
     }
     printf("\n");
   }
