50552c1d717dda766a4f7fbaf60ac7fb939930db
Po-Hsun Tseng <zengbs@gmail.com>
2021-06-13 20:53:18 +0800

    update

--- hw3/P6/main.c
+++ hw3/P6/main.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
+#include <stddef.h>
 #include<stdlib.h>
 #include<stdbool.h>
 #include<string.h>
 #include<stdint.h>
 #include<limits.h>
+
+#define VERBOSE
 
 void checkPtr(void *ptr, int line)
 {    
@@ -23,9 +26,9 @@ int main(){
   
   scanf("%d", &lengthAdjList);
 
-  int **head   = (int**)malloc((size_t)(lengthAdjList)*sizeof(int*));
-  int  *numVex = (int* )malloc((size_t)(lengthAdjList)*sizeof(int ));
-  int **Ptr    = (int**)malloc((size_t)(lengthAdjList)*sizeof(int*));
+  int **head    = (int**)malloc((size_t)(lengthAdjList)*sizeof(int*));
+  int  *numVex  = (int* )malloc((size_t)(lengthAdjList)*sizeof(int ));
+  int **Ptr     = (int**)malloc((size_t)(lengthAdjList)*sizeof(int*));
 
   /*============== STEP-1: input ============== */
   for (int i=0;i<lengthAdjList;i++){
@@ -40,46 +43,72 @@ int main(){
       head[i][j]--;
     }
 
-    *Ptr[i] = &head[i][0];
+    Ptr[i] = &head[i][0];
   }
 
   /*============== STEP-2:  ============== */
-  int i = 0;
-  int j = 0;
+  int j1 = 0;
+  int j2 = 0;
 
   int p1, p2;
 
  for(int i=0;i<lengthAdjList;i++){
 
-      p1 = i;
  
-      while ( *Ptr[*Ptr[p1]] == p1 ){
+      if ( *Ptr[*Ptr[i]] == i ){
 
+        p1 = i;
         p2 = *Ptr[p1];
 
-        printf("%d %d\n",p1, p2);
 
-        j++;
-
+        while(j1 < numVex[p1] || j2 < numVex[p2]){
 
 
-        Ptr[p1] = &head[p1][j];
-        Ptr[p2] = &head[p2][j];
+          if ( *Ptr[*Ptr[p1]] == p1 ){
+
+#           ifdef VERBOSE
+            printf("line:%d, p1=%d, p2=%d\n", __LINE__, p1, p2);
+            printf("line:%d, j1=%d, j2=%d\n", __LINE__, j1, j2);
+#           endif
+
+            p2 = *Ptr[p1];
+            j2 = (int)(Ptr[p2] - head[p2]);
+
+            j1++;
+            j2++;
+
+            Ptr[p1] = &head[p1][j1];
+            Ptr[p2] = &head[p2][j2];
+
+          }else if ( *Ptr[*Ptr[p2]] == p2 ){
+
+#           ifdef VERBOSE
+            printf("line:%d, p1=%d, p2=%d\n", __LINE__, p1, p2);
+            printf("line:%d, j1=%d, j2=%d\n", __LINE__, j1, j2);
+#           endif
+
+            p1 = *Ptr[p2];
+            j1 = (int)(Ptr[p1] - head[p1]);
+
+            j1++; 
+            j2++; 
 
 
-        if ( *Ptr[*Ptr[p1]] == p1 ){
+            Ptr[p1] = &head[p1][j1];
+            Ptr[p2] = &head[p2][j2];
 
-          p2 = *Ptr[p1];
+          }else{
+            printf("error!");
+            exit(0);
+          }
 
-        }else if ( *Ptr[*Ptr[p2]] == p2 ){
 
-          p1 = *Ptr[p2];
+          if (p1<p2) printf("%d %d\n",p1, p2);
+          else       printf("%d %d\n",p2, p1);
 
-        }else{
-          printf("error!");
-          exit(0);
+
+
         }
-
       }
 
  }
